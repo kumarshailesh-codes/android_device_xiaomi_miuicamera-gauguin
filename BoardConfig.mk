b82906fb0836dd4e2470424a46c7d52f4cc16bc0
kumarshailesh-codes <kumarshailesh12qw4@gmail.com>
2026-05-10 05:27:23 +0000

    new

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,26 @@ MALLOC_SVELTE := true
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
 
-# Sepolicy
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/private
-BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
+# Sepolicy MiSys
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(MIUICAMERA_PATH)/sepolicy/misys/vendor
+
+BOARD_PRIVATE_SEPOLICY_DIRS += \
+    $(MIUICAMERA_PATH)/sepolicy/misys/private
+
+BOARD_PUBLIC_SEPOLICY_DIRS += \
+    $(MIUICAMERA_PATH)/sepolicy/misys/public
+
+# Sepolicy Camera
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(MIUICAMERA_PATH)/sepolicy/vendor
+
+BOARD_PRIVATE_SEPOLICY_DIRS += \
+    $(MIUICAMERA_PATH)/sepolicy/private
+
+# Camera Extensions permissions
+PRODUCT_COPY_FILES += \
+    $(MIUICAMERA_PATH)/configs/permissions/extensions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/camerax-vendor-extensions.xml
+# Permissions
+PRODUCT_COPY_FILES += \
+     $(call find-copy-subdir-files,*,$(MIUICAMERA_PATH)/configs/permissions/,$(TARGET_COPY_OUT_SYSTEM)/etc)
--- device.mk
+++ device.mk
@@ -1,3 +1,36 @@
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    MiuiCamera
+
+# Shims
+PRODUCT_PACKAGES += \
+    libgui_shim_miuicamera
+
+# Logging
+PRODUCT_SYSTEM_PROPERTIES += \
+   log.tag.CHIUSECASE=ERROR
+
+PRODUCT_VENDOR_PROPERTIES += \
+   persist.vendor.camera.logInfoMask=false
+
+# Symlinks
+PRODUCT_PACKAGES += \
+    miui_camera_libcamera_algoup_symlink \
+    miui_camera_libcamera_mianode_symlink \
+    miui_camera_libmicampostproc_symlink \
+    miui_camera_campostproc_vendor_symlink \
+    miui_camera_libOpenCL_symlink \
+    miui_camera_libmqsas_symlink \
+    miui_camera_libcameraimpl_symlink \
+    miui_camera_libopencl_camera_symlink
+
+# MiSys HIDL deps
+PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0 \
+    android.hidl.memory.block@1.0.vendor \
+    android.hidl.memory@1.0-impl
 #
 # Copyright (C) 2023-2025 The LineageOS Project
 #
@@ -20,3 +53,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # Sysconfig
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
+
+PRODUCT_PACKAGES += \
+    gralloc.qcom
